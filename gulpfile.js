c92b7d3c93eafc7691502bba030f9cbd40e455df
Jeremy Attali <jeremy.attali@gmail.com>
2015-11-24 19:40:22 -0500

    chore(systemjs): bundle with systemjs

--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,19 @@ var gulp = require('gulp');
 var rename = require('gulp-rename');
 var typescript = require('gulp-typescript');
 
+var Builder = require('systemjs-builder');
 var tsProject = typescript.createProject('tsconfig.json');
 
 var config = {
   path: {
-    out: 'out',
+    src: 'src',
+    build: 'build/es5/mnubo',
     dist: 'dist'
   }
 };
 
 gulp.task('watch', function() {
-  gulp.watch(config.path.out + '/**/*.js', ['build']);
+  gulp.watch(config.path.src + '/**/*.ts', ['build']);
 });
 
 gulp.task('build', function() {
@@ -25,7 +27,45 @@ gulp.task('build', function() {
     .pipe(rename(function(path) {
       path.dirname = path.dirname.substring(4); /* src/ */
     }))
-    .pipe(gulp.dest('dist/'));
+    .pipe(gulp.dest(config.path.build));
 });
 
-gulp.task('dist', ['build'], function() { });
+gulp.task('bundle', ['build'], function() {
+  var builder = new Builder('build/es5');
+  var builderConfig = {
+    defaultJSExtensions: true,
+    paths: {
+      'lodash': './node_modules/lodash/index.js',
+    },
+    meta: {
+      'http': {
+        build: false
+      },
+      'https': {
+        build: false
+      }
+    },
+    warnings: true
+  };
+
+  var bundleSuccess = function() {
+    console.log('systemjs bundle was created');
+  };
+
+  var bundleError = function(error) {
+    console.trace();
+    console.error('error while creating systemjs bundle', error);
+  };
+
+  builder.config(builderConfig);
+
+  builder.bundle('mnubo/mnubo.js', config.path.dist + '/mnubo.js')
+    .then(bundleSuccess)
+    .catch(bundleError);
+
+  builder.buildStatic('mnubo/mnubo.js', config.path.dist + '/mnubo.static.js')
+    .then(bundleSuccess)
+    .catch(bundleError);
+});
+
+gulp.task('dist', ['bundle'], function() { });
