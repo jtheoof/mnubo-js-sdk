a4f5746a790ff5184cc7f99dcace7082cd7324cb
Jeremy Attali <jeremy.attali@gmail.com>
2015-11-27 19:15:37 -0500

    chore(gulpfile): add browserify task

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,13 @@
 'use strict';
 
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+
 var gulp = require('gulp');
 var rename = require('gulp-rename');
 var typescript = require('gulp-typescript');
+var sourcemaps = require('gulp-sourcemaps');
 
 var tsProject = typescript.createProject('tsconfig.json');
 
@@ -25,7 +30,21 @@ gulp.task('build', function() {
     .pipe(rename(function(path) {
       path.dirname = path.dirname.substring(4); /* src/ */
     }))
-    .pipe(gulp.dest('dist/'));
+    .pipe(gulp.dest(config.path.dist));
+});
+
+gulp.task('browserify', ['build'], function() {
+  var b = browserify({
+    entries: config.path.dist + '/mnubo.static.js',
+    debug: true,
+  });
+
+  return b.bundle()
+    .pipe(source('mnubo.static.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(config.path.dist + '/browserify/'));
 });
 
 gulp.task('dist', ['build'], function() { });
