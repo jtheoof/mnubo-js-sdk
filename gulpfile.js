e4e242887634fe94b80e0b0eecc6dc023262af5b
Jeremy Attali <jeremy.attali@gmail.com>
2015-11-30 14:57:23 -0500

    chore(gulpfile): add minified bundle

--- gulpfile.js
+++ gulpfile.js
@@ -8,8 +8,7 @@ var gulp = require('gulp');
 var rename = require('gulp-rename');
 var typescript = require('gulp-typescript');
 var sourcemaps = require('gulp-sourcemaps');
-
-var tsProject = typescript.createProject('tsconfig.json');
+var uglify = require('gulp-uglify');
 
 var config = {
   path: {
@@ -18,9 +17,16 @@ var config = {
   }
 };
 
-gulp.task('watch', function() {
-  gulp.watch(config.path.out + '/**/*.js', ['build']);
+var tsProject = typescript.createProject('tsconfig.json');
+
+var browserifyConfig = browserify({
+  entries: config.path.dist + '/mnubo.js',
+  debug: true,
 });
+
+var sourceMapsConfig = {
+  loadMaps: true
+};
 
 gulp.task('build', function() {
   return tsProject
@@ -33,18 +39,26 @@ gulp.task('build', function() {
     .pipe(gulp.dest(config.path.dist));
 });
 
-gulp.task('browserify', ['build'], function() {
-  var b = browserify({
-    entries: config.path.dist + '/mnubo.static.js',
-    debug: true,
-  });
-
-  return b.bundle()
-    .pipe(source('mnubo.static.js'))
+gulp.task('bundle.dev', ['build'], function() {
+  return browserifyConfig.bundle()
+    .pipe(source('mnubo.sfx.js'))
     .pipe(buffer())
-    .pipe(sourcemaps.init({loadMaps: true}))
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest(config.path.dist + '/browserify/'));
+    .pipe(sourcemaps.init(sourceMapsConfig))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(config.path.dist));
 });
+
+gulp.task('bundle.prod', ['build'], function() {
+  return browserifyConfig.bundle()
+    .pipe(source('mnubo.sfx.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init(sourceMapsConfig))
+    .pipe(uglify())
+    .pipe(rename({extname: '.min.js'}))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(config.path.dist));
+});
+
+gulp.task('bundle', ['bundle.dev', 'bundle.prod']);
 
 gulp.task('dist', ['build'], function() { });
